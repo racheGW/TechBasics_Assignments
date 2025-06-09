1d635e02de98ee4eaecd873de83d291152a48b64
Rachel Weller <Rachel.G.Weller@stud.leuphana.de>
2025-06-09 16:07:10 +0200

    Update project_week_7.py

--- assignments/week 7/project_week_7.py
+++ assignments/week 7/project_week_7.py
@@ -1,1 +1,19 @@
+Assignment 7
+Let's make a grade calculator for the class ;)
 
+Download csv file from our shared google spreadsheet for the class
+Read the csv in a way that you find meaningful (as list or dictionary?)
+Populate the csv up to week 13(no week 6) with random data
+Add two more new columns: "Total Points" & "Average Points"
+Remember "Total Points" shall be <= 30, it takes the best 10 grades from 12 assignments
+Average points can calculate average value based on all submissions
+Handle no submission and spaces
+Use Exception handling if the csv can't be found.
+Save everything as a new csv file
+Don't edit the csv file manually. I will test your code with the csv directly downloaded from the Google spreadsheet.
+Don't commit the csv file when you submit the assignment!
+
+Bonus:
+
+Display average points for stream A / stream B / each week with print()
+Beautify your print results!
